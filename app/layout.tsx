20f8b2ee59a60623fd31f7f90396d0816c00dc84
afonso burginski <afonsoburginski@gmail.com>
2023-11-06 08:21:51 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css';
+
+import { Analytics } from '@vercel/analytics/react';
+import Nav from './components/nav';import { Suspense } from 'react';
+
+export const metadata = {
+  title: 'Gerenciador de Proposta',
+  description:
+    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.'
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="h-full bg-gray-50">
+      <body className="h-full">
+        <Suspense>
+          <Nav />
+        </Suspense>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import { sql } from '@vercel/postgres';
+import { Card, Title, Text } from '@tremor/react';
+import Search from './components/search';
+
+
+export default async function IndexPage() {
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Title>Usuário</Title>
+      <Text>Gerenciador de Proposta</Text>
+      <Search />
+      <Card className="mt-6">
+        <select>
+          <option value="elevador">Elevador 1</option>
+          <option value="elevador">Elevador 2</option>
+          <option value="elevador">Elevador 3</option>
+        </select>
+
+        <select>
+          <option value="capacidade">Capacidade 1</option>
+          <option value="capacidade">Capacidade 2</option>
+          <option value="capacidade">Capacidade 3</option>
+        </select>
+
+        <select>
+          <option value="altura">Altura 1</option>
+          <option value="altura">Altura 2</option>
+          <option value="altura">Altura 3</option>
+        </select>
+      </Card>
+    </main>
+  );
+}
