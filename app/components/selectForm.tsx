949a60e846add6419cd10d809c193da1270b639a
afonsoburginski <afonsoburginski@gmail.com>
2023-11-07 16:56:01 -0300

    Final commit

--- app/components/selectForm.tsx
+++ app/components/selectForm.tsx
@@ -1,11 +1,8 @@
 /* eslint-disable react/jsx-no-undef */
 'use client';
-
 import React, { useState } from 'react';
 import Select from 'react-select';
-import { Data, productOptions, modelOptions, capacityOptions, dimensionOptions, potencyOptions, colourOptions } from '../data'
-import { SelectEstado } from './selectEstado';
-import { SelectCidade } from './selectCidade';
+import { productOptions, capacityOptions, dimensionOptions, potencyOptions } from '../data'
 
 export default function SelectForm() {
   const [isClearable, setIsClearable] = useState(true);
@@ -13,60 +10,88 @@ export default function SelectForm() {
   const [isDisabled, setIsDisabled] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isRtl, setIsRtl] = useState(false);
-  
-  const [selectedUf, setSelectedUf] = useState("");
-  console.log(selectedUf);
 
+  const [product, setProduct] = useState(null);
+  const [model, setModel] = useState(null);
+  const [capacity, setCapacity] = useState(null);
+  const [modelList, setModelList] = useState([]);
+  const [capacityList, setCapacitylList] = useState([]);
+
+  // handle change event of the product dropdown
+  const handleProductChange = (obj: React.SetStateAction<null>) => {
+    setProduct(obj);
+    setModelList(obj?.model ?? []);
+    setCapacitylList(obj?.capacity ?? []);
+    setModel(null);
+    setCapacity(null);
+  };
+
+  // handle change event of the Model dropdown
+  const handleModelChange = (obj: React.SetStateAction<null>) => {
+    setModel(obj);
+  };
+
+  const handleCapacityChange = (obj: React.SetStateAction<null>) => {
+    setCapacity(obj);
+  };
+  
   return (
     <>
     <form className='flex flex-col w-full max-w-screen-xl gap-5'>
-      {/* Primeira linha */}
       <div className='flex grid-cols-4 gap-5'>
-
         <div className='grid-cols-1 w-full'>
-        <SelectEstado onChange={setSelectedUf} />
-        <SelectCidade uf={selectedUf} />
           <label>Produto</label>
           <Select
             className="basic-single grid-cols-1 w-full"
             classNamePrefix="select"
+            placeholder="Selecionar produto"
             isDisabled={isDisabled}
             isLoading={isLoading}
             isClearable={isClearable}
             isRtl={isRtl}
             isSearchable={isSearchable}
-            name="color"
-            options={productOptions}
+            value={product} // set selected value
+            options={productOptions} // set list of the data
+            onChange={handleProductChange} // assign onChange function
+            getOptionLabel={(x) => x.label}
+            getOptionValue={(x) => x.value}
+          />
+        </div>
+        <div className='grid-cols-1 w-full'>
+          <label>Modelo</label>  
+          <Select
+            className="basic-single grid-cols-1 w-full"
+            classNamePrefix="select"
+            placeholder="Selecionar modelo"
+            isDisabled={isDisabled}
+            isLoading={isLoading}
+            isClearable={isClearable}
+            isRtl={isRtl}
+            isSearchable={isSearchable}
+            value={model} // set selected value
+            options={modelList} // set list of the data
+            onChange={handleModelChange} // assign onChange function
+            getOptionLabel={(x) => x.label}
+            getOptionValue={(x) => x.value}
           />
         </div>
 
         <div className='grid-cols-1 w-full'>
-          <label>Modelo</label>
+          <label>Capacidade</label>  
           <Select
-            className="basic-single grid-cols-3 w-full"
+            className="basic-single grid-cols-1 w-full"
             classNamePrefix="select"
+            placeholder="Capacidade"
             isDisabled={isDisabled}
             isLoading={isLoading}
             isClearable={isClearable}
             isRtl={isRtl}
             isSearchable={isSearchable}
-            name="color"
-            options={modelOptions}
-          />
-        </div>
-
-        <div className='grid-cols-1 w-full'>
-          <label>Capacidade</label>
-          <Select
-            className="basic-single grid-cols-3 w-full"
-            classNamePrefix="select"
-            isDisabled={isDisabled}
-            isLoading={isLoading}
-            isClearable={isClearable}
-            isRtl={isRtl}
-            isSearchable={isSearchable}
-            name="color"
-            options={capacityOptions}
+            value={capacity} // set selected value
+            options={capacityList} // set list of the data
+            onChange={handleCapacityChange} // assign onChange function
+            getOptionLabel={(x) => x.label}
+            getOptionValue={(x) => x.value}
           />
         </div>
 
@@ -74,7 +99,6 @@ export default function SelectForm() {
 
       {/* Segunda Linha */}
       
-
       <div className='flex grid-cols-4 gap-5'>
         <div className='grid-cols-1 w-full'>
           <label>Altura</label>
@@ -86,7 +110,6 @@ export default function SelectForm() {
             isClearable={isClearable}
             isRtl={isRtl}
             isSearchable={isSearchable}
-            name="color"
             options={dimensionOptions}
           />
         </div>
@@ -101,7 +124,6 @@ export default function SelectForm() {
             isClearable={isClearable}
             isRtl={isRtl}
             isSearchable={isSearchable}
-            name="color"
             options={potencyOptions}
           />
         </div>
@@ -115,7 +137,6 @@ export default function SelectForm() {
             isClearable={isClearable}
             isRtl={isRtl}
             isSearchable={isSearchable}
-            name="color"
             options={productOptions}
           />
         </div>
@@ -130,7 +151,6 @@ export default function SelectForm() {
             isClearable={isClearable}
             isRtl={isRtl}
             isSearchable={isSearchable}
-            name="color"
             options={productOptions}
           />
         </div>
