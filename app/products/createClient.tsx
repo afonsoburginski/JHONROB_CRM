04b076ca9ace3628a293416d9061341958660e82
afonsoburginski <afonsoburginski@gmail.com>
2023-11-24 14:20:01 -0400

    Final

--- app/products/createClient.tsx
+++ app/products/createClient.tsx
@@ -5,31 +5,17 @@ import { Button } from '@tremor/react';
 import { Form, Input } from 'reactstrap';
 
 const CreateClient = () => {
-  const formStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '10px',
-    width: '300px',
-    margin: '0 auto',
-  };
-
-  const inputStyle = {
-    padding: '10px',
-    borderRadius: '5px',
-    border: '1px solid #ccc',
-  };
-
   return (
-    <Form style={formStyle}>
-      <Input style={inputStyle} type="text" placeholder="Nome do cliente" />
-      <Input style={inputStyle} type="email" placeholder="Email do cliente" />
-      <Input style={inputStyle} type="text" placeholder="Telefone do cliente" />
-      <Input style={inputStyle} type="text" placeholder="Endereço do cliente" />
-      <Input style={inputStyle} type="text" placeholder="Cidade do cliente" />
-      <Input style={inputStyle} type="text" placeholder="Estado do cliente" />
-      <Input style={inputStyle} type="text" placeholder="CEP do cliente" />
-      <Input style={inputStyle} type="text" placeholder="CPF ou CNPJ do cliente" />
-      <Input style={inputStyle} type="text" placeholder="Inscrição Estadual do cliente" />
+    <Form className="flex flex-col space-y-4 w-72 m-auto">
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="Nome do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="email" placeholder="Email do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="Telefone do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="Endereço do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="Cidade do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="Estado do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="CEP do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="CPF ou CNPJ do cliente" />
+      <Input className="p-2 rounded border border-gray-300" type="text" placeholder="Inscrição Estadual do cliente" />
       <Button type="submit">Cadastrar Cliente</Button>
     </Form>
   );
